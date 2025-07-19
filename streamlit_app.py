bf222c540055563ad1c7463b89117c6c2643ef23
amitainz <37222107+amitainz@users.noreply.github.com>
2025-07-19 14:23:31 +0100

    Update streamlit_app.py
    sketch for bot

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,23 @@
 import streamlit as st
 
+class World(ABC):
+
+    @property
+    @abstractmethod
+    def observation_distribution(self) -> dict[str, float]
+        raise NotImplemented
+
+    @property
+    @abstractmethod
+    def possible_actions(self) -> frozenset[str]
+        raise NotImplemented
+
+    @property
+    @abstractmethod
+    def marginal_outcome_distribution(self, observation: str, action: str) -> dict[str, float]
+        raise NotImplemented
+        
+
 st.title("🎈 Making a change")
 st.write(
     "Why don't I see the change?"
