7a4db04f64227e4ba7804dedcec1ffcac3ff0c19
amitainz <37222107+amitainz@users.noreply.github.com>
2025-07-19 13:55:01 +0100

    Update streamlit_app.py
    m

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 Making a change")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Why don't I see the change?"
 )
